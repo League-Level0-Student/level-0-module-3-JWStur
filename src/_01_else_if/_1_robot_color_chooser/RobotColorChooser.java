5875faa05a106016c7883403f38b259ccb1d6ac8
root <root@e009a1696115>
2020-08-15 00:01:05 +0000

    working on robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,10 +2,42 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+ 		
+		Robot r = new Robot();
+		
+		r.setSpeed(100);
+		r.setPenWidth(10);
+		r.penDown();
+		
+		String strRed = JOptionPane.showInputDialog("How much red?");
+		String strGreen = JOptionPane.showInputDialog("How much green?");
+		String strBlue = JOptionPane.showInputDialog("How much blue?");
+		
+		if (strRed != null & strGreen != null & strBlue != null) {
+			int red = Integer.parseInt(strRed);
+			int green = Integer.parseInt(strGreen);
+			int blue = Integer.parseInt(strBlue);
+			r.setPenColor(red,green,blue);
+		
+		}
+		else {
+			int red = rand.new random
+			r.setPenColor(color);
+		}
+		
+	
+		for (int i = 1; i <= 4; i++) {
+			r.move(100);
+			r.turn(90);
+		}
+		
 		
 		//1. Create a new Robot
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
